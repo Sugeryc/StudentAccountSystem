20b196d483c20bf624c09ace709b0d2d1c21b291
yc <1653077041@qq.com>
2022-05-10 18:38:45 +0800

    学生成绩管理系统

---
+++ src/main/java/com/fc/StudentAccountSystemApplication.java
@@ -0,0 +1,13 @@
+package com.fc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentAccountSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentAccountSystemApplication.class, args);
+    }
+
+}
